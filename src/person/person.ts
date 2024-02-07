f4b515a1c30ea2e38f02c94f55aa781bc4455aa2
Stewart <44583600+stewheart123@users.noreply.github.com>
2024-02-07 21:54:16 +0000

    inital commit

---
+++ src/person/person.ts
@@ -0,0 +1,23 @@
+// Define a class
+class Person {
+    // Properties
+    firstName: string;
+    lastName: string;
+    
+    // Constructor
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    
+    // Method to get full name
+    getFullName(): string {
+        return `${this.firstName} ${this.lastName}`;
+    }
+}
+
+// Create an instance of the class
+//const person = new Person("John", "Doe");
+
+// Access properties and methods
+//console.log(person.getFullName()); // Output: John Doe
